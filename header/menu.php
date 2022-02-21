32c183b4158cff03434045310536f5c8063c309f
Yuri <yurisilva474@gmail.com>
2022-02-21 12:38:51 -0300

    menu

--- header/menu.php
+++ header/menu.php
@@ -6,7 +6,7 @@
           <i class="fas d-none fechar text-white fa-angle-left position-relative"></i>
           <img class="img-fluid" src="./img/LOGO.png" alt="">  
           <ul class="menu-principal">
-            <li class="menu-principal-item"><a class="btn btn-primary" data-bs-toggle="collapse" href="#underprotection" role="button" aria-expanded="false" aria-controls="collapseExample">Under Protection</a>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" data-bs-toggle="collapse" href="#underprotection" role="button" aria-expanded="false" aria-controls="collapseExample">Under Protection</a>
                 <ul class="sub-menu collapse" id="underprotection">
                 <!--Feitos menus e subs menus em collapse-->
                   <li class="sub-menu-item"><a href="#">Quem somos e nosso propósito</a></li></i>
@@ -18,7 +18,7 @@
                   <li class="sub-menu-item"><a href="#">Politica de Segurança da Informação</a></li>
                 </ul>
             </li>
-            <li class="menu-principal-item"><a class="btn btn-primary" data-bs-toggle="collapse" href="#solucoes" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Soluções</a>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" data-bs-toggle="collapse" href="#solucoes" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Soluções</a>
               <ul  class="sub-menu collapse" id="solucoes">
                 <li class="sub-menu-item"><a href="#">LISA</a></li>
                 <li class="sub-menu-item"><a href="#">SOC</a></li>
@@ -29,7 +29,7 @@
                 <li class="sub-menu-item"><a href="#">SecBodyShop</a></li>
               </ul>
             </li>
-            <li class="menu-principal-item"><a class="btn btn-primary" data-bs-toggle="collapse" href="#produtos" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Produtos</a>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" data-bs-toggle="collapse" href="#produtos" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Produtos</a>
               <ul class="sub-menu collapse" id="produtos">
                 <li class="sub-menu-item"><a href="#">Fortinet</a></li>
                 <li class="sub-menu-item"><a href="#">WatchGuard</a></li>
@@ -42,15 +42,15 @@
                 <li class="sub-menu-item"><a href="#">Veracode</a></li>
               </ul>
             </li>
-            <li class="menu-principal-item"><a class="btn btn-primary" data-bs-toggle="collapse" href="#relacoes" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Relações</a>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" data-bs-toggle="collapse" href="#relacoes" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Relações</a>
                 <ul class="sub-menu collapse" id="relacoes">
                   <li class="sub-menu-item"><a href="#">Nossos Clientes</a></li>
                   <li class="sub-menu-item"><a href="#">Parceiros</a></li>
                 </ul>
             </li>
-            <li class="menu-principal-item"><a class="btn btn-primary" href="#">Faça Parte da Up</a></li>
-            <li class="menu-principal-item"><a class="btn btn-primary" href="#">Noticias</a></li>
-            <li class="menu-principal-item"><a class="btn btn-primary" data-bs-toggle="collapse" href="#contatos" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Contatos</a>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" href="#">Faça Parte da Up</a></li>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" href="#">Noticias</a></li>
+            <li class="menu-principal-item"><i class="fas fa-angle-right"></i><a class="" data-bs-toggle="collapse" href="#contatos" role="button" aria-expanded="false" aria-controls="collapseExample" href="#">Contatos</a>
                 <ul class="sub-menu collapse" id="contatos">
                   <li class="sub-menu-item"><a href="#">Fale com a UP</a></li>
                   <li class="sub-menu-item"><a href="#">Fale com a DPO</a></li>
