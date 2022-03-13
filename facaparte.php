ee175f7ad0843e4dd6803a6613014cc3afa1e664
Yuri <yurisilva474@gmail.com>
2022-03-13 14:36:08 -0300

    att-desk

--- facaparte.php
+++ facaparte.php
@@ -17,10 +17,10 @@
                     <p><span> Na <strong>UP</strong>,somos movidos pelo desejo de inovar e de trazer mais proteção para as empresas através de soluções completas e equipes preparadas para lidar com os desafios.</span></p>
                 </div>
             </div>
-            <div class="col-12 texto-facaparte text-white">
-                <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h4>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class="col-12 texto-facaparte text-white ">
+                <p class="text-center mt-md-3">Nossa missão é continuamente preservar a operação, a lucratividade, a imagem das organizações e a competitividade, guiando as empresas na era da transformação digital com recursos tecnológicos eficientes em segurança. Firmamos nossas relações em parcerias duradouras em que a palavra-chave é confiança. </p>
+                <p class="text-destaque text-center">O time da UP é formado por mais de 65 profissionais comprometidos em mudar a Segurança da Informação no mercado através da inovação, da resiliência, da dedicação e da vontade de fazer a diferença na história das organizações.</p>
+                <p class="text-center">Faça parte dessa mudança e crie uma carreira de sucesso na <strong>Under Protection.</strong></p>
                 <p class="confira"><a class="text-uppercase" href="#"><strong>confira as vagas disponíveis</strong></a></p>
             </div>
         </div>
