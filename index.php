31f022cc79869ee7a58a23da152d9e935005777c
lucas.miguel <lucas.thiagomiguel@hotmail.com>
2022-04-03 14:31:17 -0300

    pagina lisa

--- index.php
+++ index.php
@@ -3,7 +3,89 @@
     include "./header/menu.php";
 
 ?>
-    
+  <main>
+   <div class="container-fluid">
+     <div class="row">
+       <div class="col-12">
+         <!-- bunner -->
+       </div>
+     </div>
+   </div>
+
+   <div class="container-fluid quem-somos">
+     <div class="row">
+       <div class="col-12">
+         <h2>UNDER
+        PROTECTION</h2>
+       </div>
+       <div class="col-12">
+         <h3>QUEM SOMOS
+          E NOSSO PROPÓSITO</h3>
+       </div>
+       <div class="col-12">
+         <p>A Under Protection nasceu em 2001 da vontade de transformar a  Segurança da Informação. Mais do que apenas uma empresa que presta serviços, nosso principal desejo é inspirar confiança, sendo a sua principal parceira. Em todo o nosso tempo de história entendemos que não basta apenas oferecer produtos e soluções, desconsiderando todo o investimento que já foi realizado.  </p>
+       </div>
+       <div class="col-12">
+         <p>
+        Assim como no xadrez, utilizamos as peças disponíveis e nos posicionamos no jogo, adaptando as nossas soluções às necessidades do seu negócio. Queremos proteger o Rei pela Torre, como na jogada do roque, dando um xeque em todas as ameaças. Para tal, utilizamos metodologias baseadas em normas reconhecidas nacional e internacionalmente, além de tecnologias de ponta, inteligência artificial e inovações para trazer toda a proteção que a sua empresa precisa.  </p>
+
+        <p>Hoje atuamos em todo o Brasil e temos uma equipe composta por mais de 65 profissionais, totalmente preparados para lidar com ameaças da atualidade, desde os incidentes cibernéticos mais avançados até as falhas de segurança internas, alheias ao meio digital.  Nosso desejo é fazer a diferença e, conosco, você terá uma guardiã contra os principais riscos e vulnerabilidades que podem afetar a sua produtividade, operação, lucratividade e a confiabilidade no mercado.  </p>
+       </div>
+     </div>
+   </div>
+   <div class="container-fluid">
+     <div class="row">
+       <div class="col-12">
+         <h4>NOSSA HISTÓRIA</h4>
+       </div>
+       <div class="col-12">
+         <img src="" alt="">
+         <p>A Under Protection tem origem no estado do Paraná e, apenas oito meses após sua inauguração, marca presença e ganha destaque em publicações de veículos especializados. </p> 
+
+         <p>Criação e lançamento do LISA®, nossa ferramenta exclusiva para análise, diagnóstico e mitigação de risco. Desde 2001, o LISA® é o carro-chefe de nossa marca.  </p>
+         <img src="" alt="">
+       </div>
+     </div>
+   </div>
+   <div class="container-fluid">
+     <div class="row">
+       <h3>Com a UP, você tem uma parceira de negócios e guardiã contra os principais riscos e vulnerabilidades que podem afetar a sua empresa.  </h3>
+     </div>
+     <div class="col-12">
+       <img src="" alt="">
+       <h2>MISSÃO</h2>
+       <div class="">
+         <p>Desenvolver e aplicar planos de segurança de acordo com as necessidades de cada cliente, atendendo a todos os requisitos legais.  </p>
+       </div>
+     </div>
+   </div>
+   <div class="container-fluid">
+     <div class="row">
+       <div class="col-12">
+          <h3>Por que escolher a 
+          Under Protection?</h3>
+        </div>
+        <div class="col-12">
+         <h2>X</h2>
+         <p>ataques monitorados 
+         pelo SOC</p>
+        </div>
+        <div class="col-12">
+          <p>Sabemos que uma proteção completa vai além da aplicação de recursos cibernéticos, como antivírus e firewalls. Eles são muito importantes e necessários ao negócio, mas para uma segurança efetiva e de qualidade, é preciso unir:  </p>
+        </div>
+        <div class="col-12">
+
+        </div>
+     </div>
+   </div>
+   <div class="container-fluid">
+     <div class="row">
+       <div class="col-12">
+         <p>Na UP, acumulamos anos de experiência no mercado de tecnologia para trazer soluções eficientes em consultoria, monitoramento, conscientização de equipes, sistema de gestão de SI e respostas a incidentes, oferecendo uma proteção completa e dinâmica ao seu ambiente. Em conjunto com empresas parceiras, tecnologias de ponta e equipes preparadas para solucionar os incidentes, proporcionamos a melhora na qualidade da segurança da sua empresa.</p>
+       </div>
+     </div>
+   </div>
+ </main>
 
 
 <?php   
---
+++ lisa.php
@@ -0,0 +1,87 @@
+<?php 
+    include 'header/index.php';
+    include 'header/menu.php';
+?>
+<main>
+    -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 ">
+                <h1>SOLUÇÕES
+                COMPLETAS</h1>
+            </div>
+            <div class="col-12">
+                <a href="">LISA®</a>
+                <a href="">SOC</a>
+                <a href="">PRODUTOS</a>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 ">
+                <h2> CONSULTORIA</h2>
+            </div>
+            <div class="col-12">
+                <p>Conhecer as vulnerabilidades do seu ambiente é o primeiro passo para desenvolver um plano de segurança eficaz e focar corretamente os investimentos. </p>
+            </div>
+            <div class="col-12">
+                <img src="" alt="">
+            </div>
+            <div class="col-12">
+                <p>A consultoria da Under Protection compreende a avaliação e a identificação de brechas que podem ser utilizadas por ameaças, com o propósito de mitigar os riscos através de soluções assertivas e eficientes, a partir de testes, análises e o acompanhamento constante de nossas equipes.  </p>
+            </div>
+            <div class="col-12">
+                <img src="" alt="">
+            </div>
+            <div class="col-12">
+                <p>
+            O LISA® (Levantamento de Informações Sobre o Ambiente) é a solução exclusiva de diagnóstico, avaliação e mitigação de riscos da Under Protection.  Mais do que uma consultoria, o LISA® possibilita a análise de todo o ambiente corporativo para identificar e reduzir as vulnerabilidades que ameaçam a integridade da sua empresa, a partir de soluções eficientes que auxiliam no investimento assertivo de recursos, de acordo com as necessidades do seu negócio.  </p>
+
+            <p>Desenvolvido com base em diversas metodologias reconhecidas e nas melhores práticas, o LISA® testa o seu ambiente frente à </p>
+            </div>
+            <div class="col-12">
+                <a href="">TECNOLOGIA</a>
+                <a href="">PESSOAS</a>
+                <a href="">PROCESSOS</a>
+                <a href="">LGPD</a>
+            </div>
+
+            <div class="col-12">
+                <p>identificando e propondo soluções a todas as brechas que tornam a sua empresa vulnerável a ataques, vazamentos de dados e penalizações por órgãos reguladores.  </p>
+            </div>
+            <div class="col-12">
+                <h3>O LISA® desafia:  </h3>
+            </div>
+            <div class="">
+                <p>TECNOLOGIA</p>
+                <p>PROCESSOS</p>
+                <p>PESSOAS</p>
+                <p>LGPD</p>
+            </div>
+            <div class="box">
+                <p>TECNOLOGIA</p>
+                <div class="">
+                    <p>Análise dos servidores, banco de dados, aplicações Web, ativos de rede, quebras de senhas, scan de vulnerabilidade interno e externo, pentest. </p>
+                </div>
+            </div>
+            <div class="col-12">
+                <p>Com a aplicação de uma série de frameworks de segurança, o LISA® executa uma avaliação de riscos e de exposição aprofundada que inclui e vai além das vulnerabilidades identificadas em um vulnerability assessment e dos resultados de um Pentest. </p>
+            </div>
+            <div class="col-12">
+                <img src="" alt="">
+            </div>
+            <div class="col-12">
+                <p>O LISA® possibilita o mapeamento, a análise e a apresentação de soluções para todas as ameaças que podem afetar a operação, a lucratividade, a confiabilidade e a reputação da sua empresa no mercado, a partir do diagnóstico objetivo dos problemas existentes e da proposta de investimentos assertivos em curto, médio e longo prazo.   </p>
+            </div>
+        </div>
+    </div>
+   
+
+</main>
+
+
+
+<?php 
+    include 'footer/index.php';
+?>
