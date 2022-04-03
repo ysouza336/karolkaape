22f206afde0699d33a8a99166e0efdd81f6c601e
lucas.miguel <lucas.thiagomiguel@hotmail.com>
2022-04-03 17:28:07 -0300

    arrumando lisa

--- lisa.php
+++ lisa.php
@@ -3,61 +3,70 @@
     include 'header/menu.php';
 ?>
 <main>
-    -->
-    <div class="container-fluid">
+    <div class="container-fluid funso-lisa">
         <div class="row">
-            <div class="col-12 ">
+            <div class="col-12 text-center">
                 <h1>SOLUÇÕES
-                COMPLETAS</h1>
+                    <strong>
+                COMPLETAS</strong>
+            </h1>
             </div>
-            <div class="col-12">
+            <div class="btn-lisa d-flex align-items-center justify-content-around mt-3">
                 <a href="">LISA®</a>
                 <a href="">SOC</a>
                 <a href="">PRODUTOS</a>
             </div>
         </div>
     </div>
-    <div class="container-fluid">
+    <div class="container-fluid consultoria-lisa pt-4">
         <div class="row">
-            <div class="col-12 ">
+            <div class="col-12 text-center">
                 <h2> CONSULTORIA</h2>
             </div>
-            <div class="col-12">
+            <div class="col-12 text-center lisa-text-1">
                 <p>Conhecer as vulnerabilidades do seu ambiente é o primeiro passo para desenvolver um plano de segurança eficaz e focar corretamente os investimentos. </p>
             </div>
-            <div class="col-12">
-                <img src="" alt="">
+            <div class="col-12 mt-5">
+                <img class="img-fluid" src="img/icones/lisa-icone.png" alt="">
             </div>
-            <div class="col-12">
+            <div class="col-12 text-center mt-5">
                 <p>A consultoria da Under Protection compreende a avaliação e a identificação de brechas que podem ser utilizadas por ameaças, com o propósito de mitigar os riscos através de soluções assertivas e eficientes, a partir de testes, análises e o acompanhamento constante de nossas equipes.  </p>
             </div>
-            <div class="col-12">
-                <img src="" alt="">
+            <div class="col-12 text-center mt-5">
+                <img class="img-fluid" src="img/icones/lisa-torre.png" alt="">
             </div>
-            <div class="col-12">
+            <div class="col-12 text-center mt-5">
                 <p>
             O LISA® (Levantamento de Informações Sobre o Ambiente) é a solução exclusiva de diagnóstico, avaliação e mitigação de riscos da Under Protection.  Mais do que uma consultoria, o LISA® possibilita a análise de todo o ambiente corporativo para identificar e reduzir as vulnerabilidades que ameaçam a integridade da sua empresa, a partir de soluções eficientes que auxiliam no investimento assertivo de recursos, de acordo com as necessidades do seu negócio.  </p>
 
             <p>Desenvolvido com base em diversas metodologias reconhecidas e nas melhores práticas, o LISA® testa o seu ambiente frente à </p>
             </div>
-            <div class="col-12">
-                <a href="">TECNOLOGIA</a>
-                <a href="">PESSOAS</a>
-                <a href="">PROCESSOS</a>
-                <a href="">LGPD</a>
+            <div class="col-12 text-center d-flex justify-content-around align-items-center flex-wrap mt-3">
+                <a href="" class="mb-3 ">TECNOLOGIA</a>
+                <a href="" class="mb-3 ">PESSOAS</a>
+                <a href="" class="">PROCESSOS</a>
+                <a href="" class="">LGPD</a>
             </div>
 
-            <div class="col-12">
+            <div class="col-12 text-center mt-5">
                 <p>identificando e propondo soluções a todas as brechas que tornam a sua empresa vulnerável a ataques, vazamentos de dados e penalizações por órgãos reguladores.  </p>
             </div>
-            <div class="col-12">
+            <div class="col-12 text-center">
                 <h3>O LISA® desafia:  </h3>
             </div>
-            <div class="">
-                <p>TECNOLOGIA</p>
-                <p>PROCESSOS</p>
-                <p>PESSOAS</p>
-                <p>LGPD</p>
+            <div class="circo-btn-lisa">
+                <div class="">
+                    <p class="text-center">TECNOLOGIA</p>
+                </div>
+                <div class="">
+                    <p class="text-center">PROCESSOS</p>
+                </div>
+                <div class="">
+                    <p class="text-center">PESSOAS</p>
+                </div>
+                <div class="">
+                    <p class="text-center">LGPD</p>
+                </div>
             </div>
             <div class="box">
                 <p>TECNOLOGIA</p>
